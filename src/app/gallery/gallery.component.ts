6d0600c61aed6abb3b0af5ae8c15796aa98b1a2e
karmakarvishal <35589480+karmakarvishal@users.noreply.github.com>
2021-06-26 16:39:44 +0530

    Photo update

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -57,22 +57,22 @@ export class GalleryComponent implements OnInit {
   ];
 
   tiles: Tile[] = [
-    { imgDescription: 'Image1', text: 'One', cols: 3, rows: 2, color: 'lightblue', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
-    { imgDescription: 'Image2', text: 'Two', cols: 3, rows: 2, color: 'lightgreen', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
-    { imgDescription: 'Image3', text: 'Three', cols: 3, rows: 2, color: 'lightpink', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
-    { imgDescription: 'Image4', text: 'Four', cols: 3, rows: 2, color: '#DDBDF1', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
-    { imgDescription: 'Image5', text: 'One', cols: 3, rows: 2, color: 'lightblue', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
-    { imgDescription: 'Image6', text: 'Two', cols: 3, rows: 2, color: 'lightgreen', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
-    { imgDescription: 'Image7', text: 'Three', cols: 3, rows: 2, color: 'lightpink', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
-    { imgDescription: 'Image8', text: 'Four', cols: 3, rows: 2, color: '#DDBDF1', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
-    { imgDescription: 'Image9', text: 'One', cols: 3, rows: 2, color: 'lightblue', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
-    { imgDescription: 'Image10', text: 'Two', cols: 3, rows: 2, color: 'lightgreen', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
-    { imgDescription: 'Image11', text: 'Three', cols: 3, rows: 2, color: 'lightpink', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
-    { imgDescription: 'Image12', text: 'Four', cols: 3, rows: 2, color: '#DDBDF1', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
-    { imgDescription: 'Image13', text: 'One', cols: 3, rows: 2, color: 'lightblue', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
-    { imgDescription: 'Image14', text: 'Two', cols: 3, rows: 2, color: 'lightgreen', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
-    { imgDescription: 'Image15', text: 'Three', cols: 3, rows: 2, color: 'lightpink', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
-    { imgDescription: 'Image16', text: 'Four', cols: 3, rows: 2, color: '#DDBDF1', img: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg' },
+    { imgDescription: 'Image1', text: 'One', cols: 3, rows: 2, color: 'lightblue', img: 'https://www.formula1.com/content/fom-website/en/teams/Red-Bull-Racing/_jcr_content/gallery/image2.img.1024.medium.jpg/1615472582711.jpg' },
+    { imgDescription: 'Image2', text: 'Two', cols: 3, rows: 2, color: 'lightgreen', img: 'https://www.formula1.com/content/fom-website/en/teams/Red-Bull-Racing/_jcr_content/gallery/image3.img.1024.medium.jpg/1615542187198.jpg' },
+    { imgDescription: 'Image3', text: 'Three', cols: 3, rows: 2, color: 'lightpink', img: 'https://www.formula1.com/content/fom-website/en/teams/Red-Bull-Racing/_jcr_content/gallery/image11.img.1024.medium.jpg/1615542041703.jpg' },
+    { imgDescription: 'Image4', text: 'Four', cols: 3, rows: 2, color: '#DDBDF1', img: 'https://www.formula1.com/content/fom-website/en/teams/Red-Bull-Racing/_jcr_content/gallery/image1.img.1024.medium.jpg/1615542616551.jpg' },
+    { imgDescription: 'Image5', text: 'One', cols: 3, rows: 2, color: 'lightblue', img: 'https://www.formula1.com/content/fom-website/en/teams/Red-Bull-Racing/_jcr_content/gallery/image4.img.1024.medium.jpg/1615548771873.jpg' },
+    { imgDescription: 'Image6', text: 'Two', cols: 3, rows: 2, color: 'lightgreen', img: 'https://www.formula1.com/content/fom-website/en/teams/Ferrari/_jcr_content/gallery/image3.img.1024.medium.jpg/1615481825982.jpg' },
+    { imgDescription: 'Image7', text: 'Three', cols: 3, rows: 2, color: 'lightpink', img: 'https://www.formula1.com/content/fom-website/en/teams/Ferrari/_jcr_content/gallery/image1.img.1024.medium.jpg/1615541232656.jpg' },
+    { imgDescription: 'Image8', text: 'Four', cols: 3, rows: 2, color: '#DDBDF1', img: 'https://www.formula1.com/content/fom-website/en/teams/Ferrari/_jcr_content/gallery/image51.img.1024.medium.jpg/1582905824701.jpg' },
+    { imgDescription: 'Image9', text: 'One', cols: 3, rows: 2, color: 'lightblue', img: 'https://www.formula1.com/content/fom-website/en/teams/Ferrari/_jcr_content/gallery/image5.img.1024.medium..png/1615480846514.png' },
+    { imgDescription: 'Image10', text: 'Two', cols: 3, rows: 2, color: 'lightgreen', img: 'https://www.formula1.com/content/fom-website/en/teams/Mercedes/_jcr_content/gallery/image1.img.1024.medium.jpg/1594574372910.jpg' },
+    { imgDescription: 'Image11', text: 'Three', cols: 3, rows: 2, color: 'lightpink', img: 'https://www.formula1.com/content/fom-website/en/teams/Mercedes/_jcr_content/gallery/image51.img.1024.medium.jpg/1593959216311.jpg' },
+    { imgDescription: 'Image12', text: 'Four', cols: 3, rows: 2, color: '#DDBDF1', img: 'https://www.formula1.com/content/fom-website/en/teams/Mercedes/_jcr_content/gallery/image5.img.1024.medium.jpg/1594567069782.jpg' },
+    { imgDescription: 'Image13', text: 'One', cols: 3, rows: 2, color: 'lightblue', img: 'https://www.formula1.com/content/fom-website/en/teams/Mercedes/_jcr_content/gallery/image3.img.1024.medium.jpg/1594577126522.jpg' },
+    { imgDescription: 'Image14', text: 'Two', cols: 3, rows: 2, color: 'lightgreen', img: 'https://www.formula1.com/content/fom-website/en/teams/Aston-Martin/_jcr_content/gallery/image61.img.1024.medium.jpg/1614942356336.jpg' },
+    { imgDescription: 'Image15', text: 'Three', cols: 3, rows: 2, color: 'lightpink', img: 'https://www.formula1.com/content/fom-website/en/teams/Aston-Martin/_jcr_content/gallery/image1.img.1024.medium.jpg/1614784697198.jpg' },
+    { imgDescription: 'Image16', text: 'Four', cols: 3, rows: 2, color: '#DDBDF1', img: 'https://www.formula1.com/content/fom-website/en/teams/Aston-Martin/_jcr_content/gallery/image5.img.1024.medium.jpg/1614784697611.jpg' },
   ];
 
 
